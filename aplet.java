2d9469fd9734f747f9fe420c184b32905ad83266
Naveenreddy290 <nallamarunaveen123@gmail.com>
2021-04-28 14:36:14 +0530

    sreenu

---
+++ aplet.java
@@ -0,0 +1,12 @@
+import java.io.*;
+ import java.awt.*;
+ import java.applet.Applet; 
+/*<applet code="App1.class" height=100 width=500></applet>*/
+ public class App1 extends Applet 
+{ 
+ public void paint(Graphics g)  
+{  
+ g.drawString("MCA III semester Students",50,60);   
+setForeground(Color.blue);  
+} 
+} 
